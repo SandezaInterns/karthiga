d54cc3bfbf88fcfcfef23b153b7c6bc4f911638b
SandezaInterns <ramasubramaniankarthiga@gmail.com>
2025-05-21 11:50:21 +0530

    Time: 4 ms (73.2%), Space: 60.7 MB (70.3%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const roman={
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let total=0
+     for(i=0;i<s.length;i++){
+        current=roman[s[i]]
+        next=roman[s[i+1]]
+
+     
+        if(next>current){
+           total -=current
+     }
+        else{
+           total +=current
+     }
+     }
+     return total;
+};
